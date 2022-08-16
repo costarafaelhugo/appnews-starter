33465da754fb6e863a683dbe4d0242cff7ab2987
Hugo Costa <costarafaelhugo@gmail.com>
2022-08-16 09:16:10 -0300

    Create Models

--- app/src/main/java/daniellopes/io/newsappstarter/SearchActivity.kt
+++ app/src/main/java/daniellopes/io/newsappstarter/SearchActivity.kt
@@ -1,4 +1,4 @@
-package daniellopes
+package daniellopes.io.newsappstarter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/daniellopes/io/newsappstarter/model/Source.kt
@@ -0,0 +1,11 @@
+package daniellopes.io.newsappstarter.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Source(
+    @SerializedName("id")
+    val id: Any,
+    @SerializedName("name")
+    val name: String
+)
