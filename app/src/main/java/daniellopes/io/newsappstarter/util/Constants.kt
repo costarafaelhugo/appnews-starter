202da66a5a2a34bb27bfd3750772f5bc002e8a9d
Hugo Costa <costarafaelhugo@gmail.com>
2022-08-16 09:41:31 -0300

    Create and configure endpoints and retrofit

---
+++ app/src/main/java/daniellopes/io/newsappstarter/util/Constants.kt
@@ -0,0 +1,9 @@
+package daniellopes.io.newsappstarter.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "80aea1070d95402380f34a0ad7059200"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
